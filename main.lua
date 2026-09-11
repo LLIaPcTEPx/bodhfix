a464bb173494bb82714c2121e3984f75caac1469
LLIaPcTEPx <x3osemailconfirming@gmail.com>
2026-09-11 10:55:49 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,5 +5,6 @@ shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Performance%
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Trickshot%20%26%20Movement")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Sky%20changer")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Streamer%20Mode")
+shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Legit Speed Glitch")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/CODM%20Guns")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/RTX%20%26%20Graphics")

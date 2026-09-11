7a3690baa2ed9e47e30e42d8e691c3d10371f70a
LLIaPcTEPx <x3osemailconfirming@gmail.com>
2026-09-11 11:03:33 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,6 @@ shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/UI%20%26%20F
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Performance%20%26%20FPS")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Trickshot%20%26%20Movement")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Performance Overlay")
-shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Troll%20(FE)")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Sky%20changer")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Privacy%20and%20security")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Gun sound Changer")

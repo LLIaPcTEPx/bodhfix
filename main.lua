d21bd1421c561de75d2fa7db32914dcf0091fc17
LLIaPcTEPx <x3osemailconfirming@gmail.com>
2026-09-11 11:08:47 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,9 @@
 local shared = odh_shared_plugins
 
-shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Customization")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/UI%20%26%20Fonts")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Performance%20%26%20FPS")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Trickshot%20%26%20Movement")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Sky%20changer")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Streamer%20Mode")
-shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/CODM%20Guns")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/RTX%20%26%20Graphics")
-shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Credits")
+
